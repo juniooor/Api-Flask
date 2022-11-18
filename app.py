f1e759cba230960f92ea2dc70ef21885cf2d1835
Juniooor <juniormodern@hotmail.com>
2022-11-17 21:49:43 -0300

    adcionando codigo

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, Response, Request
+from flask_sqlalchemy import SQLAlchemy
+import mysql.connector
+import json
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/crud'
+
+db = SQLAlchemy(app)
+
+class Usuario(db.Model):
+    id = db.Column(db.Integer, primary_key= True)
+    nome = db.Column(db.String(50))
+    email = db.Column(db.String(400))
+    
